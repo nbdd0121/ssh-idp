9b2bafbf285742c98d529b62edda00ffd47ebaef
Gary Guo <gary@garyguo.net>
2024-09-22 18:39:52 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    # Need newer Rust, switch to 24.11 after release cut.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "ssh-idp";
+          version = "0.1.0";
+
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "jsonwebtoken-9.3.0" = "sha256-9N4kjOAAth0t2xRLnzuWl/wngee9SAnlI/rOPYcdtkE=";
+              "pageant-0.0.1-beta.3" = "sha256-EC7yGPUIVlN2N++IchAvbtaWK+x9kV9v7oUhElw2WZY=";
+            };
+          };
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
