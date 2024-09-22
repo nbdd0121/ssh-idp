192d02c4ecf511ef00444210923c7667dedd3df6
Gary Guo <gary@garyguo.net>
2024-09-22 19:39:32 +0100

    Add docker image build to nix

--- flake.nix
+++ flake.nix
@@ -16,18 +16,49 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        inherit (pkgs) lib;
+        fs = lib.fileset;
       in
       {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "ssh-idp";
-          version = "0.1.0";
+        packages = rec {
+          default = ssh-idp;
 
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              "jsonwebtoken-9.3.0" = "sha256-ZDdT3JW9mABZDBTkzMaHml1VbZryIJDsuoEz1vSDRhE=";
-              "pageant-0.0.1-beta.3" = "sha256-EC7yGPUIVlN2N++IchAvbtaWK+x9kV9v7oUhElw2WZY=";
+          ssh-idp = pkgs.rustPlatform.buildRustPackage {
+            pname = "ssh-idp";
+            version = "0.1.0";
+
+            src = fs.toSource {
+              root = ./.;
+              fileset = fs.unions [
+                ./Cargo.toml
+                ./Cargo.lock
+                ./src
+              ];
+            };
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "jsonwebtoken-9.3.0" = "sha256-ZDdT3JW9mABZDBTkzMaHml1VbZryIJDsuoEz1vSDRhE=";
+                "pageant-0.0.1-beta.3" = "sha256-EC7yGPUIVlN2N++IchAvbtaWK+x9kV9v7oUhElw2WZY=";
+              };
+            };
+
+            meta = {
+              mainProgram = "ssh-idp";
+            };
+          };
+
+          docker = pkgs.dockerTools.buildImage {
+            name = "ssh-idp-docker";
+            tag = "latest";
+            copyToRoot = [
+              (pkgs.runCommandLocal "" { } ''
+                mkdir -p $out/work
+              '')
+            ];
+            config = {
+              Entrypoint = [ (lib.getExe ssh-idp) ];
+              WorkingDir = "/work";
             };
           };
         };
